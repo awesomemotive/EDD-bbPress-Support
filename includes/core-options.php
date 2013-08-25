894da6443ca83a782659319832a0587e77f10027
Pippin Williamson <pippin@pippinsplugins.com>
2013-08-25 19:25:13 -0400

    remove unsed enabled functions

--- includes/core-options.php
+++ includes/core-options.php
@@ -8,8 +8,6 @@ function edd_bbp_d_add_options() {
 	$options = array (
 		'_bbps_default_status'            => '1',
 		'_bbps_enable_post_count'         => '1',
-		'_bbps_enable_user_rank'          => '1',
-		'_bbps_status_permissions'        => '',
 		'_bbps_reply_count'               => '',
 		'_bbps_used_status'               => '',
 		'_bbps_enable_topic_move'         => '1',
@@ -26,9 +24,6 @@ function edd_bbp_d_is_post_count_enabled(){
 	return get_option( '_bbps_enable_post_count' );
 }
 
-function edd_bbp_d_is_user_rank_enabled(){
-	return get_option( '_bbps_enable_user_rank' );
-}
 
 function edd_bbp_d_is_resolved_enabled(){
 	$options = get_option( '_bbps_used_status' );
@@ -63,22 +58,10 @@ function edd_bbp_d_is_topic_move_enabled(){
 	return get_option( '_bbps_enable_topic_move' );
 }
 
-function edd_bbp_d_is_topic_urgent_enabled(){
-	return get_option( '_bbps_status_permissions_urgent' );
-}
-
-function edd_bbp_d_is_topic_claim_enabled(){
-	return get_option( '_bbps_claim_topic' );
-}
-
 function edd_bbp_d_is_topic_claim_display_enabled(){
 	return get_option( '_bbps_claim_topic_display' );
 }
 
 function edd_bbp_d_is_topic_assign_enabled(){
 	return get_option( '_bbps_topic_assign' );
-}
-
-function edd_bbp_d_is_user_trusted_enabled(){
-	return get_option( '_bbps_enable_trusted_tag' );
 }
