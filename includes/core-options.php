f215fa92b7e8bdfa5e1e36765d607da8570e453e
Pippin Williamson <pippin@pippinsplugins.com>
2013-08-25 19:19:03 -0400

    Fix undefined indexes on settings

--- includes/core-options.php
+++ includes/core-options.php
@@ -32,33 +32,32 @@ function edd_bbp_d_is_user_rank_enabled(){
 
 function edd_bbp_d_is_resolved_enabled(){
 	$options = get_option( '_bbps_used_status' );
-	return $options['res'];
+	return isset( $options['res'] ) ? $options['res'] : false;
 }
 
 function edd_bbp_d_is_not_resolved_enabled(){
 	$options = get_option( '_bbps_used_status' );
-	return $options['notres'];
+	return isset( $options['notres'] ) ? $options['notres'] : false;
 }
 
 function edd_bbp_d_is_not_support_enabled(){
 	$options = get_option( '_bbps_used_status' );
-	return $options['notsup'];
+	return isset( $options['notsup'] ) ? $options['notsup'] : false;
 }
 
 function edd_bbp_d_is_moderator_enabled(){
 	$options = get_option( '_bbps_status_permissions' );
-	return $options['mod'];
+	return isset( $options['mod'] ) ? $options['mod'] : false;
 }
 
 function edd_bbp_d_is_admin_enabled(){
 	$options = get_option( '_bbps_status_permissions' );
-	return $options['admin'];
+	return isset( $options['admin'] ) ? $options['admin'] : false;
 }
 
 function edd_bbp_d_is_user_enabled(){
 	$options = get_option( '_bbps_status_permissions' );
-	return $options['user'];
-}
+	return isset( $options['user'] ) ? $options['user'] : false;}
 
 function edd_bbp_d_is_topic_move_enabled(){
 	return get_option( '_bbps_enable_topic_move' );
