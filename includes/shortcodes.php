3a9afa0c0114d55835ae542319777b3db6bc705f
Pippin Williamson <pippin@pippinsplugins.com>
2014-10-02 16:24:31 -0500

    Pass the current user ID

--- includes/shortcodes.php
+++ includes/shortcodes.php
@@ -439,7 +439,7 @@ add_shortcode( 'bbps_dashboard', 'edd_bbp_dashboard_shortcode' );
 
 function edd_bbp_support_tickets( $atts, $content = null ) {
 
-	$tickets = bbp_get_user_topics_started();
+	$tickets = bbp_get_user_topics_started( get_current_user_id() );
 
 	if( $tickets ) :
 ?>
