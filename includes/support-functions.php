19ad0ff742f0e6106c414832a7a9d14a80c3d364
Pippin Williamson <pippin@pippinsplugins.com>
2014-10-06 21:37:47 -0500

    Div tags around the labels. #38

--- includes/support-functions.php
+++ includes/support-functions.php
@@ -905,12 +905,16 @@ function edd_bbp_common_issues() {
 			<option value="fes-upload">File upload error in Frontend Submissions</option>
 		</select>
 		<div>
-			<label for="edd-bbp-docs-no-help">
-				<input type="radio" id="edd-bbp-docs-no-help" name="edd-bbp-docs-help" value="1"/> I read the documentation but it did not help
-			</label>
-			<label for="edd-bbp-no-docs">
-				<input type="radio" id="edd-bbp-no-docs" name="edd-bbp-docs-help" value="2"/> I did not find any documentation about my issue
-			</label>
+			<div>
+				<label for="edd-bbp-docs-no-help">
+					<input type="radio" id="edd-bbp-docs-no-help" name="edd-bbp-docs-help" value="1"/> I read the documentation but it did not help
+				</label>
+			</div>
+			<div>
+				<label for="edd-bbp-no-docs">
+					<input type="radio" id="edd-bbp-no-docs" name="edd-bbp-docs-help" value="2"/> I did not find any documentation about my issue
+				</label>
+			</div>
 		</div>
 		<div id="edd-common-ticket-answers">
 			<div id="edd-common-issue-install-extension" class="bbp-template-notice" style="display:none;">
